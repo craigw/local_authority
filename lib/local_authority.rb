ba0ee92d6eb21d60e8c3c3346d644fceaaf6286d
Craig R Webster <craig@barkingiguana.com>
2012-10-07 20:55:53 +0100

    Let people access the DB

--- lib/local_authority.rb
+++ lib/local_authority.rb
@@ -1,5 +1,46 @@
 require "local_authority/version"
+require "csv"
 
 module LocalAuthority
-  # Your code goes here...
+  class LocalAuthority
+    DB_FILE = File.join File.dirname(__FILE__), '..', 'db', 'local_authorities.csv'
+    DB = CSV.new File.read(DB_FILE), :headers => :first_row
+
+    def self.all
+      @all ||= DB.map { |row| new row.to_hash }
+    end
+
+    def self.find_by_map_it_id id
+      all.detect { |la|
+        la.map_it_id =~ /\/#{id}$/
+      }
+    end
+
+    attr_accessor :attributes
+    private :attributes=
+
+    def initialize attributes
+      self.attributes = attributes
+    end
+
+    def website
+      attributes['Website']
+    end
+
+    def name
+      attributes['Name']
+    end
+
+    def phone_number
+      attributes['Phone Number']
+    end
+
+    def address
+      attributes['Address']
+    end
+
+    def map_it_id
+      attributes['MapIt ID']
+    end
+  end
 end
